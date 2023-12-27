483733772404ad13c0443d1c7b3967f260089171
208cai5099 <208cai5099@gmail.com>
2023-12-27 13:25:08 -0500

    Set up env file

---
+++ server/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+// set up a pool to connect to database
+const pool = new Pool({
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DB
+})
+
+module.exports = pool;
---
+++ server/index.js
@@ -0,0 +1,4 @@
+import express from "express";
+
+// set up server
+const server = express();
