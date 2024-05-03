0738cd5ebea9e841443d33d2ea503a8485c33256
208cai5099 <208cai5099@gmail.com>
2024-05-03 14:40:36 -0400

    removed url and key

--- client/src/config/supabase.js
+++ client/src/config/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://tbtaiasgpklkrifwkqii.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRidGFpYXNncGtsa3JpZndrcWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MDgwNTEsImV4cCI6MjAzMDI4NDA1MX0.lJxQQASlhJzoNs9I1ymxbtknVjBLuDmb6TxpQ07wYCg'
+const supabaseUrl = ''
+const supabaseKey = ''
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
