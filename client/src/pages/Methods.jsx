2d5bbce020859a5bcfc4dd7b85421ef0d0d0da19
208cai5099 <208cai5099@gmail.com>
2024-05-03 22:42:32 -0400

    removed env this time

--- client/src/pages/Methods.jsx
+++ client/src/pages/Methods.jsx
@@ -20,7 +20,7 @@ function Methods() {
                 </p>
 
                 <p>
-                    The downloaded Excel dataset includes Regents results from 2015 to 2023. As explained the dataset, the dataset only takes into 
+                    The downloaded Excel dataset includes Regents results from 2015 to 2023. As explained in the notes within the Excel file, the dataset only takes into 
                     account of a student's highest score on a specific exam in a given year. For example, a student who took the Chemistry Regents exam
                     more than once in 2016 would only have their highest grade be factored into their school's mean score and other metrics in
                     the year 2016. The Excel dataset aggregated the results by multiple categories, such as gender, English Language Learner status, and
@@ -35,7 +35,7 @@ function Methods() {
                     For our purposes, we extracted the following data: school DBN (unique school identifier), school name, year, regents exam, total 
                     number of test takers, mean scores, and percent of students scoring 65% or above. We also noticed that some school names were
                     truncated or misspelled. So, we referenced the NYC DOE website as well as the schools' personal websites (if they exist) to confirm 
-                    the full correct spelling of the names.
+                    the full correct spelling of their names.
                 </p>
 
                 <p>
@@ -46,7 +46,7 @@ function Methods() {
                 <p>
                     In 2021, only four Regents exams were offered due to COVID restrictions: Living Environment, Earth Science, Common Core Algebra, 
                     and Common Core English. For some reason, the dataset listed two schools that took the Global History Regents. We suspect that this
-                    was a mistake, so we removed the scores for those two schools.
+                    was a mistake, so we removed the scores from those two schools.
                 </p>
 
                 <h2>
@@ -54,7 +54,15 @@ function Methods() {
                 </h2>
 
                 <p>
-                    Talk about SQL data storage on AWS
+                    We are using <a href="https://supabase.com/" target="_blank">Supabase</a> as our backend. In our backend, the data are organized into four SQL tables: regents, schools, yearly_avg_, and borough_avg.
+
+                    regents: stores every school's mean score and passing rate on the Regents exams
+
+                    schools: stores every pair of school dbn and school name
+
+                    yearly_avg: stores every borough's yearly average scores on the Regents exams
+
+                    borough_avg: stpres every borough's average scores on the Regents exams across all available years
                 </p>
 
                 <h2>
