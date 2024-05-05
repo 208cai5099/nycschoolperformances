8ab94967cb9dd1d76919fecdf9dbc55cecc96b6b
Zhuo Biao Cai <208cai5099@gmail.com>
2024-05-05 18:58:36 -0400

    Edited contact

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -14,7 +14,8 @@ function Contact() {
                     biology to middle school students in Brooklyn, followed by teaching biology to high school students in Manhattan. When I taught my former 
                     8th graders, I realized that it would be very helpful for families and students to have a central place to compare school's academic 
                     performances. Now that I've learned a few things about programming, I thought it would be nice to put those skills into practice. Thus, this 
-                    website was born. Hope you enjoy using it!    
+                    website was born. Hope you enjoy using it!
+                    Email: 208cai5099@gmail.com
                     </p>
                 </Panel>
             </Col>
@@ -22,10 +23,7 @@ function Contact() {
             <Col sm={10} md={10} lg={10}>
                 <Panel className="panel" header="Katrina Shih" bordered={true}>
                     <p className="body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-                    anim id est laborum.
+                    Coming soon!
                     </p>
                 </Panel>
             </Col>
