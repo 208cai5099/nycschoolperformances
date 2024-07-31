59c1d03c937421b2eb1cc7a2921044ef609d0969
208cai5099 <208cai5099@gmail.com>
2024-07-30 23:33:33 -0400

    Formatted the website

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Contact.css"
-import { Panel, Row, Col } from "rsuite";
+import { Panel, Row, Col} from "rsuite";
 
 function Contact() {
 
     return (
 
-        <Row id="contactLayout">
-            <Col sm={10} md={10} lg={10}>
-                <Panel className="panel" header="Leven Cai" bordered={true}>
-                    <p className="bio">
+
+            <Col id="panelLayout">
+                <Panel className="bioPanel" bordered={true}>
+
+                    <h3>Zhuo Biao (Leven) Cai</h3>
+                    <p>
                     I'm a current student in the Master of Computer and Information Technology program at University of Pennsylvania. I used to teach math and 
                     biology to middle school students in Brooklyn, followed by teaching biology to high school students in Manhattan. When I taught my former 
                     8th graders, I realized that it would be very helpful for families and students to have a central place to compare school's academic 
@@ -21,17 +23,17 @@ function Contact() {
                     Email: 208cai5099@gmail.com
                     </p>
                 </Panel>
-            </Col>
 
-            <Col sm={10} md={10} lg={10}>
-                <Panel className="panel" header="Katrina Shih" bordered={true}>
-                    <p className="body">
+                <Panel className="bioPanel" bordered={true}>
+                    <h3>Katrina Shih</h3>
+
+                    <p>
                     Coming soon!
                     </p>
                 </Panel>
+
             </Col>
 
-        </Row>
 
   
     )
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -5,18 +5,25 @@ function Welcome() {
 
   return (
 
-    <div className="title">
-      <h1>Welcome!</h1>
+    <div>
+      <div className="title">
 
-      <p>This website is designed to provide information about the performances of NYC schools on the annual NYS Regents exams. </p>
-      <p>Click <span className="highlight">Explore</span> to get started.</p>
+        <h1>Welcome!</h1>
+
+        <p>This website is designed to provide information about the performances of NYC schools on the annual NYS Regents exams. </p>
+        
+        <p>Click <span className="highlight">Explore</span> to get started.</p>
+
+      </div>
+
       <img 
         src = "https://cdn.pixabay.com/photo/2020/10/04/04/41/school-5625219_1280.png" 
         alt='school'
         className="school"
-        />
+      />
 
       <footer className="footer">Made with ❤️ in NYC</footer>
+
 
     </div>
 
